87f4f53b247ccbd18cd95082a7e391d0c7a59bde
Linus <cryl28@cbchs.org>
2024-12-16 15:15:38 -0600

    sirtdt_iter

---
+++ users_cyr.py
@@ -0,0 +1,30 @@
+class User:
+    def __init__(self, number, age, first_name, last_name):
+        self.age = age
+        
+        self.number = number
+        
+        self.first_name = first_name
+        
+        self.last_name = last_name
+
+    def describe_user(self):
+        print(f"{self.first_name} {self.last_name} is {self.age} years old and is {self.number} feet tall.")
+
+    def greet_user(self):
+        print(f"Hello, how are you doing, {self.first_name} {self.last_name}.")
+
+        
+user = User("fifteen", "six", "Linus", "Cyr")
+user.greet_user()
+user.describe_user()
+
+attributes = User(14, 5, )
+attributes.describe_user()
+
+
+# print(f"The user is {user.age}")
+# print(f"The user is {user.number}")
+
+
+
