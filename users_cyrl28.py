c53b222d44d19d4b2348eaf9139dcc92e94751d4
Linus <cryl28@cbchs.org>
2024-12-15 22:59:33 -0600

    Created a pyhton file about classes

---
+++ users_cyrl28.py
@@ -0,0 +1,22 @@
+class user:
+    def _init_(self, first_name, last_name, home_town, height ):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.home_town = home_town
+        self.height = height
+
+    def describe_user(self):
+        print(f"{self.first_name} {last_name} lives in {home_town}.")
+
+    def user_height(self):
+        print(f"{self.first_name} {last_name} is {height} feet tall")
+
+        User = user(f"The resturant is {resturant_name} is open and it serves {food_type}.")
+
+resturant_name = user."Five Guys"
+food_type = user("burgers")
+
+
+
+
+
---
+++ weather-class-assignment-cyrl28.py
@@ -0,0 +1,30 @@
+class Weather:
+    def _init_(self, temperature, humidity):
+        #initialize attributes about the weather
+        self.temperature = temperature
+        self.humidity = humidity
+
+    def display_conditions(self):
+        #print a statement telling the user about the conditions outside
+        print(f"{self.temerature} is the cureent temperature outside and {humidity} is the current humidity level.")
+    
+    def is_rainy(self):
+        #tell the user if it is raining or not
+        return self.humidity > 70
+        print(f"{self.humidity} is the current humidity that is making it rainy.")
+        return self.humidity < 70
+        print(f"{self.humidity} is the current humidity that is not making it rainy.")
+
+        temperature = Weather(55)
+        humidity = Weather(80)
+
+    def temperature_change(change):
+        #tell the user about the temperature change
+        self.temperature = temperature_change(-3)
+        print(f"{self.temperature} is now the current temperature.")
+
+print(class Weather)
+
+
+    
+
